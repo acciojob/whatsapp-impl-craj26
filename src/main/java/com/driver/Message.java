c3daf35dd4c9d708100c6a3adbd16aaaacf6ab48
Arpit <arpitjain7254@gmail.com>
2023-03-17 13:34:11 +0530

    3

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -1,18 +1,24 @@
 package com.driver;
-
 import java.util.Date;
 
 public class Message {
     private int id;
     private String content;
     private Date timestamp;
+    public Message(int id) {
+        this.id = id;
+        this.timestamp = new Date();
+    }
 
-
-
-    public Message(int id, String content, Date timestamp) {
+    public Message(int id, String content) {
         this.id = id;
         this.content = content;
         this.timestamp = new Date();
+    }
+    public Message(int id, String content, Date timestamp) {
+        this.id = id;
+        this.content = content;
+        this.timestamp = timestamp;
     }
 
     public int getId() {
