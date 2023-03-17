c95467a9a8e6d59022602b96d559576b0a0c1e80
Arpit <arpitjain7254@gmail.com>
2023-03-17 13:31:59 +0530

    2

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -7,16 +7,12 @@ public class Message {
     private String content;
     private Date timestamp;
 
-    public Message(int id, String content) {
-        this.id = id;
-        this.content = content;
-        this.timestamp = new Date();
-    }
+
 
     public Message(int id, String content, Date timestamp) {
         this.id = id;
         this.content = content;
-        this.timestamp = timestamp;
+        this.timestamp = new Date();
     }
 
     public int getId() {
